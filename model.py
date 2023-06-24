092415f92b5c5635e4460ad91c94c19b44ec8b8a
Ankush Goel <77546629+AnkushGoel251@users.noreply.github.com>
2023-06-24 06:53:23 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,20 +1,23 @@
 import pandas as pd
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.linear_model import LogisticRegression
 import pickle
 
 
-df = pd.read_csv("Mall_Customers.csv") # Heart-disease
-df.drop(["CustomerID"], axis =1 , inplace = True)
-X1=df.loc[:, ["Age", "Spending Score (1-100)"]].values
+df = pd.read_csv("heart-disease.csv") # Heart-disease
 
-
+# split data into train and test
+x = df.drop("target" , axis = 1)
+y = df["target"]
+np.random.seed(42)
 
 # split into train and test 
-#Initialize the class object
-kmeans = KMeans (n_clusters= 4)
-#predict the Labels of clusters.
+x_train , x_test , y_train , y_test = train_test_split(x,y , test_size=0.2)
 
-model = kmeans.fit_predict(X1)
+
+#Initialize the class object
+model = LogisticRegression(max_iter = 120)
+model.fit(x_train , y_train)
+
 # Pickle
 pickle.dump(model,open("model.pkl","ab"))    
